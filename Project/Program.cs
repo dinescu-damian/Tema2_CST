688b6fd04ad4ab0916c093d2c0454266d7e49231
Dinescu Damian <dandreidamian@gmail.com>
2023-04-22 11:49:48 +0300

    Add project files.

---
+++ Project/Program.cs
@@ -0,0 +1,34 @@
+using Project.Settings;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+Dependencies.Inject(builder);
+
+var app = builder.Build();
+
+/*
+app.UseSwagger(c =>
+{
+    c.RouteTemplate = "api/swagger/{documentName}/swagger.json";
+});
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/api/swagger/v1/swagger.json", "Project");
+    c.RoutePrefix = "api/swagger";
+});
+*/
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
